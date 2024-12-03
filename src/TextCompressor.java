14d19fa33660d70179389d33a3633a5f2e2393db
soccer47 <stevie.halprin@menloschool.org>
2024-12-03 14:33:09 -0800

    commit

--- src/TextCompressor.java
+++ src/TextCompressor.java
@@ -33,7 +33,18 @@ import java.util.LinkedHashMap;
  */
 public class TextCompressor {
 
-    public static final int ESCAPE_CODE = 3;
+    // Length of binary codes representing the first index of the word, and then the length of the word
+    public static final int CODE_LENGTH = 21;
+    // Length of header holding length of the text
+    public static final int TEXT_LENGTH = 30;
+    // Length of binary codes representing the first index of the word
+    public static final int CODE_LENGTH_INDEX = 14;
+    // Length of binary codes representing the length of the word
+    public static final int CODE_LENGTH_LEN = 5;
+    // Length of the binary codes representing individual chars
+    public static final int LETTER_LENGTH = 6;
+    // Boolean to represent if the current mode is writing out characters
+    public static boolean charMode = true;
 
 
     private static void compress() {
@@ -45,11 +56,9 @@ public class TextCompressor {
         // Hashmap to hold the first occurrence of every word printed at least once
         HashMap<String, Integer> wordStarts = new LinkedHashMap<>();
 
+        // Write out the length of the original String in TEXT_LENGTH characters
+        BinaryStdOut.write(text.length(), TEXT_LENGTH);
 
-        // Integer to hold the number of words added to the HashMap so far
-        int numWords = 0;
-        // Boolean to represent if the current mode is writing out characters
-        boolean charMode = true;
 
         // Go through every word in the text array
         for (String word : textWords) {
@@ -60,13 +69,19 @@ public class TextCompressor {
                 if (charMode) {
                     switchMode(charMode);
                 }
+                // Then print the code referencing the first instance of the word in the String
+                writeCode(wordStarts.get(word), word.length());
             }
-            // Otherwise switch to char mode with escape code if needed
+            // Otherwise write out the word character by character into the binary file
             else {
+                // Switch to char mode with escape code if needed
                 if (!charMode) {
                     switchMode(charMode);
                 }
-                BinaryStdOut.write(word);
+                // Add the word to the HashMap of already printed words
+                wordStarts.put(word, text.indexOf(word));
+                // Then write out the word into the binary file
+                writeWord(word);
             }
         }
 
@@ -75,7 +90,21 @@ public class TextCompressor {
 
     private static void expand() {
 
-        // TODO: Complete the expand() method
+        // Get the length of the original text from the header of the binary file
+        int finalLength = BinaryStdIn.readInt(TEXT_LENGTH);
+        // String holding output of compressed binary file
+        String text = "";
+        // Boolean representing if the current mode being used in the file is reading characters or word codes
+        // Initialized to true because first word cannot have been stated before in text
+        boolean isCharMode = true;
+        // Length of binary codes being read in
+        int codeLength = LETTER_LENGTH;
+        // Escape code for letter codes
+
+
+        while (text.length() <= finalLength) {
+
+        }
 
         BinaryStdOut.close();
     }
@@ -91,19 +120,40 @@ public class TextCompressor {
     // Method to write out the code to switch the current mode in the binary file
     private static void switchMode(boolean isCharMode) {
         // Length of escape code to be written
-        int length = 8;
+        int length = CODE_LENGTH;
         // If the mode is currently char mode and is to be switched to code mode, alter the length of the escape code
         if (isCharMode) {
-            length = 6;
+            length = LETTER_LENGTH;
         }
+        // Write the escape code into the binary file
         BinaryStdOut.write(0, length);
+        // Switch the mode of being used in the file accordingly
+        charMode = !charMode;
     }
 
     // Write out the word in 6 bit binary codes
     private static void writeWord(String word) {
+        // Go through every character in the word, and print each out in
         for (int i = 0; i < word.length(); i++) {
-            BinaryStdOut.write((int)word.charAt(i), 6);
+            // Get the next character in the word
+            char c = word.charAt(i);
+            // A-Z will be written as 1-26 respectively
+            if (!Character.isLowerCase(c)) {
+                BinaryStdOut.write((int)c - 64, 6);
+            }
+            // a-z will be written as 27-52 respectively
+            else {
+                BinaryStdOut.write((int)c - 70, 6);
+            }
         }
+    }
+
+    // Write out the word in 6 bit binary codes
+    private static void writeCode(int firstIndex, int wordLength) {
+        // Write out the index where the word first occurs in the text
+        BinaryStdOut.write(firstIndex, CODE_LENGTH_INDEX);
+        // Then write out the length of the word
+        BinaryStdOut.write(wordLength, CODE_LENGTH_LEN);
     }
 
     public static void main(String[] args) {
